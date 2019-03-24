dcc60e7223be256629a36ecc21c785ee90b9b92f
Bartosz Kaminski <bakaminski@wp.pl>
2019-03-24 15:04:42 +0100

    Updates

---
+++ precache-manifest.cb7795b3047d768ef9f4abf24b2695ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0ca7acd365ec61085ce",
+    "url": "/Weather-App/static/css/main.be6a3a7e.chunk.css"
+  },
+  {
+    "revision": "b0ca7acd365ec61085ce",
+    "url": "/Weather-App/static/js/main.cc763f8b.chunk.js"
+  },
+  {
+    "revision": "89bb695666ed5c38f231",
+    "url": "/Weather-App/static/js/runtime~main.671df5a1.js"
+  },
+  {
+    "revision": "3c688a83c6a96b1bd515",
+    "url": "/Weather-App/static/js/2.f885ce20.chunk.js"
+  },
+  {
+    "revision": "3c2b8bf7191a6a70a282eb3f6db4ee3b",
+    "url": "/Weather-App/static/media/bgc.3c2b8bf7.jpg"
+  },
+  {
+    "revision": "e69296f7a6ea180e9198c3329672b450",
+    "url": "/Weather-App/index.html"
+  }
+];
